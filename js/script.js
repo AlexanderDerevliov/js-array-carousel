bb8a49f04c343bbae331470848afdcac56d95c89
AlexanderDerevliov <alex.feelred@gmail.com>
2023-10-25 11:01:33 +0200

    improved exercise with teacher correction

--- js/script.js
+++ js/script.js
@@ -1,26 +1,57 @@
 
 // creo un ARRAY
-const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
-
-const up = document.querySelector ("up-side");
-const down = document.querySelector ("down-side");
+const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
 
 
+// cercare ed inserire elemento items
 
-let curentImageIndex = 0;
+const itemsElem = document.querySelector(".items");
 
 
-//per ogni elemento dell array creo un tag img
-for(let i = 0; i < images.length; i++){
-    const curImage = images [i]
-    imagesString += `<img class="slide-image" src="${curImage}" alt="">`;
+
+// scorrere array
+//  creare div con immagine dentro e aggiungere una stringa
+let imagesString = "";
+for (let i = 0; i < images.length; i++) {
+    imagesString += `
+    <div class="item">
+        <img src="./img/${images[i]}" alt="">
+     </div>`;
 }
 
-console.log (imagesString)
-
-slideElem.innerHTML = imagesString; 
+console.log(imagesString)
 
 
-const slideElems = document.querySelectorAll ("slide-image");
-console.log (slideElems)
+
+// aggiungere stringa contenete tutti i div con le img all'interno dell'elenco "items"
+
+itemsElem.innerHTML = itemsElem.innerHTML + imagesString;
+
+
+// impostare lo slider
+
+let currentIndex = 0;
+const slideElems = document.querySelectorAll(".item");
+slideElems[currentIndex].classList.add("active");
+
+
+// gestire il click sulla freccia su
+document.querySelector(".up-side").addEventListener("click", function(){
+      
+    if (currentIndex < slideElems.length - 1) {
+        slideElems[currentIndex].classList.remove("active")
+        currentIndex++;
+        slideElems[currentIndex].classList.add("active")
+    }
+
+})
+
+document.querySelector(".down-side").addEventListener("click", function(){
+    if (currentIndex > 0) {
+        slideElems[currentIndex].classList.remove("active")
+        currentIndex--;
+        slideElems[currentIndex].classList.add("active")
+    }
+
+})
 
